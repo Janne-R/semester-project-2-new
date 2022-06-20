8e430bb8ea7c7a1574e313f22c94056c8521eadf
Janne-R <74651035+Janne-R@users.noreply.github.com>
2022-06-20 09:40:39 +0200

    pageTitle h1 done

--- src/components/common/PageTitle.js
+++ src/components/common/PageTitle.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
 const H1 = styled.h1`
-color: ${props => props.primary ? ({ theme }) => theme.colors.textColorDark : ({ theme }) => theme.colors.textColorLight};
+color: ${props => props.primaryColor ? ({ theme }) => theme.colors.textColorDark : ({ theme }) => theme.colors.textColorLight};
 `;
 
 const PageTitle = ({ title, primary }) => {
   return (
-    <H1 primary={primary}>{title}</H1>
+    <H1 primaryColor={primary}>{title}</H1>
   );
 };
 
