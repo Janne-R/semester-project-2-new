f5fd5ebeda8f73255151dbef58f4382ef04372c5
Janne-R <74651035+Janne-R@users.noreply.github.com>
2022-06-22 14:06:20 +0200

    posts form styling done

--- src/components/pages/admin/PostForm.js
+++ src/components/pages/admin/PostForm.js
@@ -6,26 +6,36 @@ import { FiEdit } from 'react-icons/fi';
 import { ImBin } from 'react-icons/im';
 
 
-
-
-const PostContainer = styled(Container)`
-margin: 100px 10px 0 10px;
-border: 1px solid ${({ theme }) => theme.colors.primaryColor};
-border-radius: 5px;
+const Background = styled.div`
+  background-color:${({ theme }) => theme.colors.backgroundColorLight};
+  margin: 100px 10px 100px 10px;
+  border: 1px solid ${({ theme }) => theme.colors.primaryColor};
+  border-radius: 5px;
 `;
 
 const Flex = styled.div`
- display: grid;
-    grid-template-columns: 3fr 1fr;
-    align-items: baseline;
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  align-items: baseline;
+  padding: 10px;
+  
 `;
 
 const Grid = styled.div`
+ background-color:${({ theme }) => theme.colors.backgroundColorLight};
+ :nth-of-type(even){
+  background-color:${({ theme }) => theme.colors.backgroundColor};
+ }
+ padding: 10px;
   display: grid;
   grid-template-columns: 1fr 1fr;
   @media ${({ theme }) => theme.devices.tabletS} { 
   grid-template-columns: 1fr 1fr 1fr 1fr;
   }
+`;
+
+const GridHeader = styled(Grid)`
+  background-color:${({ theme }) => theme.colors.backgroundColor};
 `;
 
 const FormButton = styled.button`
@@ -40,26 +50,35 @@ font-family: 'Open Sans',sans-serif;
 }
 `;
 
+
+
 const PostForm = () => {
   return (
-    <PostContainer>
-      <Flex>
-        <H2 primary title="Posts" />
-        <Button width={"150px"} text="Add new" />
-      </Flex>
-      <Grid>
-        <H3 primary uppercase title="Id" />
-        <H3 primary uppercase title="Post title" />
-      </Grid>
-      <Grid>
+    <Container>
+      <Background>
+        <Flex>
+          <H2 primary title="Posts" />
+          <Button width={"150px"} text="Add new" />
+        </Flex>
+        <GridHeader>
+          <H3 primary uppercase title="Id" />
+          <H3 primary uppercase title="Post title" />
+        </GridHeader>
+        <Grid>
+          <P primary paragraph="Id" />
+          <P primary paragraph="Title" />
+          <FormButton>Edit <FiEdit /></FormButton>
+          <FormButton>Delete <ImBin /></FormButton>
+        </Grid>
+        <Grid>
+          <P primary paragraph="Id" />
+          <P primary paragraph="Title" />
+          <FormButton>Edit <FiEdit /></FormButton>
+          <FormButton>Delete <ImBin /></FormButton>
+        </Grid>
+      </Background>
+    </Container>
 
-        <P primary paragraph="Id" />
-        <P primary paragraph="Title" />
-
-        <FormButton>Edit <FiEdit /></FormButton>
-        <FormButton>Delete <ImBin /></FormButton>
-      </Grid>
-    </PostContainer>
   )
 }
 
