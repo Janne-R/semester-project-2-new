28643cdf306d6e353ecd860367ade12d6bcb7110
Janne-R <74651035+Janne-R@users.noreply.github.com>
2022-07-06 16:01:56 +0200

    folder sturcture

---
+++ src/components/pages/home/PostsIntro.js
@@ -0,0 +1,35 @@
+import Container from "../../common/Container";
+import { H1, P } from "../../common/DisplayText"
+import styled from "styled-components";
+import Illustration from "../../common/Illustration";
+
+const Div = styled.div`
+ background-color: ${({ theme }) => theme.colors.primaryColor};
+`;
+
+const Flex = styled.div`
+  @media ${({ theme }) => theme.devices.tabletS} { 
+    display: flex;
+    flex-direction: space-between;
+  }
+`;
+
+const PostsIntro = () => {
+  return (
+    <Div>
+      <Container>
+        <Flex>
+          <div>
+            <H1 title="Browse our posts" />
+            <P paragraph="Welcome to Code Stack, this is the wiki-page for
+          front-end development. Browse all our post
+          below or try the search filter."/>
+          </div>
+          <Illustration src="images/codeDrawing.png" alt="image" height="200px" />
+        </Flex>
+      </Container>
+    </Div>
+  );
+};
+
+export default PostsIntro;
